488d254e2d593e4fa062a7a5269b64cfecdeaa71
hertbex <mlive654@gmail.com>
2022-09-27 22:21:18 +0500

    started rendering part

--- src/components/Advantages/Advantages.jsx
+++ src/components/Advantages/Advantages.jsx
@@ -2,12 +2,14 @@ import './Advantages.css'
 import Treeshaking from "../Treeshaking/Treeshaking";
 import Spa from "../Spa/Spa"
 import React from 'react';
+import Rendering from "../Rendering/Rendering";
 export default function Advantages(){
     return(
         <div className={"Advantages"}>
             <h1>В чем приемущества?</h1>
             <Treeshaking/>
             <Spa/>
+            <Rendering/>
         </div>
     )
 }
